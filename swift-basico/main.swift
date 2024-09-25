6269dec055da171516f79ae9900da85055faea68
Arturo alumno <arturo_garcia_m-tsapp1oa2324@cev.com>
2024-09-25 09:10:43 +0100

    Añadido comprobación de máxima longitud, que no comience por número y no contenga caracteres especiales

--- swift-basico/main.swift
+++ swift-basico/main.swift
@@ -1,11 +1,49 @@
-//
-//  main.swift
-//  swift-basico
-//
-//  Created by user234563 on 9/19/24.
-//
+/*
+var num = 2
+print(num)
+*/
 
-import Foundation
+//principalmente las variables las ponemos como constantes
 
-print("Hello, Wooooorld!")
+print("Hola, ¿cómo te llamas?: ")
 
+//Estructura de una función que devuelve un booleano y recibe un string
+func tieneCaracteresEspeciales(texto: String) -> Bool
+{
+    let especiales = "!¡$%/()?¿+-*,.;:{}[]"
+    for letra in texto {
+        for especial in especiales {
+            if letra == especial {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+var nombre = readLine()!
+var nombreIncorrecto = false
+repeat {
+    if nombre.count > 10 //Máximo 10 letras
+    {
+        print("ERROR: nombre demasiado largo")
+        nombreIncorrecto = true
+        nombre = readLine()!
+    } else if nombre.first!.isNumber //No empezar por número
+    {
+        print("ERROR: el nombre no puede empezar por número")
+        nombreIncorrecto = true
+        nombre = readLine()!
+    }
+    else if tieneCaracteresEspeciales(texto: nombre) //Sin caracteres especiales
+    {
+        print("ERROR: el nombre no puede contener caracteres especiales")
+        nombreIncorrecto = true
+        nombre = readLine()!
+    }
+    else
+    {
+        nombreIncorrecto = false
+    }
+} while nombreIncorrecto
+print("Hola", nombre)
